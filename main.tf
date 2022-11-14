bc4f49d8063ec76b0469788af6b9634a0a4f4afb
Chris Pollock <73022650+ChristopherPollock@users.noreply.github.com>
2022-11-13 23:28:10 -0500

    Added singleton webserver with busybox and "Hello,World!" Webpage

--- main.tf
+++ main.tf
@@ -1,8 +1,27 @@
 resource "aws_instance" "example" {
-    ami ="ami-08c40ec9ead489470"
-    instance_type = "t2.micro"
+  ami                    = "ami-08c40ec9ead489470"
+  instance_type          = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.instance.id]
 
-    tags = {
-        Name = "Terraform"
-    }
+  user_data                   = <<-EOF
+            #!/bin/bash
+            echo "Hello, World!" > index.html
+            nohup busybox httpd -f -p 8080 &
+            EOF
+
+  user_data_replace_on_change = true
+
+  tags = {
+    Name = "Terraform"
+  }
+}
+
+resource "aws_security_group" "instance" {
+  name = "terraform-example-instance"
+  ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
